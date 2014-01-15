0b985312a3a20058be4c287912d24d64f806038b
Federico Mon <gnu.fede@gmail.com>
2014-01-15 20:51:40 +0100

    Branch and bound version

--- pycifras.py
+++ pycifras.py
@@ -1,5 +1,82 @@
 #!/usr/bin/env python3
 import sys, getopt, itertools
+import math
+
+states = dict()
+results = dict()
+
+def addToDic(dic, key, value):
+    if key not in dic:
+        dic[key] = []
+        dic[key].append(value)
+    else:
+        dic[key].append(value)
+
+def addOrNotToDic(dic, key, value):
+    if len(dic):
+        if key < list(dic.keys())[0]:
+            dic.clear()
+            dic[key] = []
+            dic[key].append(value)
+    else:
+        dic[key] = []
+        dic[key].append(value)
+
+class State():
+    global states
+    global results
+
+    def __init__(self, expected_res, numbers, operation, operations):
+        self.expected_res = expected_res
+        self.operation = operation
+        self.result = self.oper()
+        opers = operations[::]
+        tempoper = '='.join((operation,str(self.result))),
+        opers.append(tempoper)
+        self.operations = opers
+        self.numbers = numbers
+        self.numbers.append(self.result)
+        addOrNotToDic(results, int(self.distanceToResult()), self)
+        addToDic(states, str(self.numbers.sort()), self)
+        if self.result != 0:
+            self.iterate()
+
+    def distanceToResult(self):
+        return math.fabs(self.expected_res-self.result)
+
+    def oper(self):
+        return eval(self.operation)
+
+    def __unicode__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return str(self.operations)
+
+    def inStates(self):
+        return str(self.numbers.sort()) in results
+
+    def iterate(self):
+        #if self.distanceToResult:
+        if not self.inStates():
+            for oper in "+*/-":
+                for x,y in itertools.combinations(self.numbers, 2):
+                    tempnumbers = self.numbers[::]
+                    tempnumbers.remove(x)
+                    tempnumbers.remove(y)
+                    child = State(self.expected_res,
+                            tempnumbers,
+                            oper.join((str(x),str(y))),
+                            self.operations)
+        else:
+            print(self.operations)
+
+def compute(result, arguments):
+    intargs = [int(x) for x in arguments]
+    state = State(expected_res=int(result), numbers=intargs, operation='1', 
+            operations=list())
+    return [str(x)+': '+str(y[0]) for x,y in results.items() if len(y)]
+#    print(results[0].operations)
 
 def main(argv):
     try:
@@ -13,9 +90,6 @@ def main(argv):
           sys.exit()
     if len(args) > 1:
         print( '\n'.join(compute(args[0], args[1:])) )
-
-def compute(target_number, numbers):
-    return [ ''.join(map(str,[it for p in zip(num_perm, oper) for it in p ]+[num_perm[-1]])) for i in range(1,len(numbers)) for oper in itertools.product(['+','-','*','/'],repeat=i) for num_perm in itertools.permutations(numbers, i+1) if eval(''.join(map(str,[it for p in zip(num_perm, oper) for it in p] + [num_perm[-1]])))==int(target_number)]
 
 if __name__ == '__main__':
     main(sys.argv[1:])
