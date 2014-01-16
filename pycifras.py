a9a04b2c967ab3320995e2fde89320735a925814
Federico Mon <gnu.fede@gmail.com>
2014-01-16 10:22:51 +0100

    Polished version

--- pycifras.py
+++ pycifras.py
@@ -4,47 +4,52 @@ import math
 
 states = dict()
 results = dict()
+expected_res = 0
 
-def addToDic(dic, key, value):
-    if key not in dic:
-        dic[key] = value
+def addToStates(key, value):
+    global states
+    if key not in states:
+        states[key] = value
         return True
     else:
         return False
 
-def addOrNotToDic(dic, key, value):
-    if len(dic):
-        if key < list(dic.keys())[0]:
-            dic.clear()
-            dic[key] = []
-            dic[key].append(value)
+def addToResults(key, value):
+    global results
+    if len(results):
+        if key < list(results.keys())[0]:
+            results.clear()
+            results[key] = []
+            results[key].append(value)
     else:
-        dic[key] = []
-        dic[key].append(value)
+        results[key] = []
+        results[key].append(value)
 
 class State():
     global states
     global results
+    global expected_res
 
-    def __init__(self, expected_res, numbers, operation, operations):
+    def __init__(self, numbers, operations, operation=None):
         self.numbers = numbers
-        self.expected_res = expected_res
-        self.result = None
         self.operations = operations
         self.operation = operation
-        self.result = self.oper()
-        opers = operations[::]
-        tempoper = '='.join((operation,str(self.result))),
-        opers.append(tempoper)
-        self.operations = opers
-        self.numbers.append(self.result)
-        addOrNotToDic(results, int(self.distanceToResult()), self)
-        if addToDic(states, str(self.numbers.sort()), self):
-            if self.result == None or self.result != 0:
+        self.result = 1
+        if operation:
+            self.result = self.oper()
+            opers = operations[::]
+            tempoper = '='.join((operation,str(self.result))),
+            opers.append(tempoper[0])
+            self.operations = opers
+            self.numbers.append(self.result)
+            self.numbers.sort()
+            addToResults(int(self.distanceToResult()), self)
+        if addToStates(str(self.numbers), self):
+            if self.result != 0:
                 self.iterate()
 
     def distanceToResult(self):
-        return math.fabs(self.expected_res-self.result)
+        return math.fabs(expected_res-self.result)
 
     def oper(self):
         return eval(self.operation)
@@ -60,20 +65,20 @@ class State():
 
     def iterate(self):
         #if self.distanceToResult:
-        for oper in "+*/-":
+        for operator in "+*/-":
             for x,y in itertools.combinations(self.numbers, 2):
                 tempnumbers = self.numbers[::]
                 tempnumbers.remove(x)
                 tempnumbers.remove(y)
-                child = State(self.expected_res,
-                        tempnumbers,
-                        oper.join((str(x),str(y))),
-                        self.operations)
+                child = State(numbers=tempnumbers,
+                        operations=self.operations,
+                        operation=operator.join((str(x),str(y))))
 
 def compute(result, arguments):
+    global expected_res
     intargs = [int(x) for x in arguments]
-    state = State(expected_res=int(result), numbers=intargs, operation='1',
-            operations=list())
+    expected_res = int(result)
+    state = State(numbers=intargs, operations=list())
     return [str(x)+': '+str(y[0]) for x,y in results.items() if len(y)]
 #    print(results[0].operations)
 
